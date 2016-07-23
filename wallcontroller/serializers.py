b668573b90f98ded3436e78539b2dde27de0a688
Khasan Khafizov <xozzslip@gmail.com>
2016-07-23 21:57:29 +0300

    created first serializer CommunitySerializer. Some tests also created

---
+++ wallcontroller/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Community
+
+
+class CommunitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Community
+        fields = ('url', 'title', 'domen', 'domen_name')
--- wallcontroller/tests.py
+++ wallcontroller/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .serializers import CommunitySerializer
+from .models import Community
+
+
+class SerializersTestCase(TestCase):
+    def setUp(self):
+        self.user = User(username="kek", password="kokkokok1", email="k@k.com")
+
+    def test_community_setializer(self):
+        community = Community(url="ff", domen_name="ff", user_owner=self.user)
+        serializer = CommunitySerializer(community)
+        self.assertTrue(len(serializer.data) > 0)
